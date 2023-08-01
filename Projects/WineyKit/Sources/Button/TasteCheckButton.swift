673d8cd2226dd8c52af326ff8e1ce1fb778c3586
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-08-01 23:39:54 +0900

    [WIN-47][#4]️✨SignUpButton, TasteCheckButton 생성

---
+++ Projects/WineyKit/Sources/Button/TasteCheckButton.swift
@@ -0,0 +1,48 @@
+//
+//  TasteCheckButton.swift
+//  WineyKit
+//
+//  Created by 박혜운 on 2023/08/01.
+//  Copyright © 2023 com.adultOfNineteen. All rights reserved.
+//
+
+import SwiftUI
+
+public struct TasteCheckButton: View {
+  let mainTitle: String
+  let subTitle: String
+  let action: () -> Void
+  
+  public init(
+    mainTitle: String,
+    subTitle: String,
+    action: @escaping () -> Void
+  ){
+    self.mainTitle = mainTitle
+    self.subTitle = subTitle
+    self.action = action
+  }
+  
+  public var body: some View {
+    Button("", action: action)
+    .buttonStyle(
+      TasteCheckButtonStyle(
+        mainTitle: self.mainTitle,
+        subTitle: self.subTitle)
+    )
+  }
+}
+
+// MARK: - 사용예시
+
+struct TasteCheckButton_Previews: PreviewProvider {
+  @State static var buttonState = true
+  static var previews: some View {
+    TasteCheckButton(
+      mainTitle: "밀크 초콜릿",
+      subTitle: "안달면 초콜릿을 왜 먹어?",
+      action: {}
+    )
+    .frame(width: 153)
+  }
+}
