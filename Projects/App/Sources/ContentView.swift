d7b2b89de3233b9853c1bf99865195da074516c2
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-08-01 23:40:06 +0900

    [WIN-47][#4]️✏️주석 추가 및 파일 삭제

--- Projects/App/Sources/ContentView.swift
+++ Projects/App/Sources/ContentView.swift
@@ -11,8 +11,13 @@ import WineyKit //폰트 정보
 
 struct ContentView: View {
   var body: some View {
-    Text("aa")
-      .wineyFont(.captionM2) //폰트 설정법
+    VStack{
+      TasteCheckButton( //의존성 테스트 코드입니다, 삭제 예정 
+        mainTitle: "밀크 초콜릿",
+        subTitle: "안달면 초콜릿을 왜 먹어?",
+        action: {}
+      )
+    }
   }
 }
 
--- Projects/WineyKit/Sources/WineyKit.swift
+++ Projects/WineyKit/Sources/WineyKit.swift
@@ -1,18 +1,9 @@
 import Foundation
 import Utils
 
-//Winey의 디자인 컴포넌트를 모아 두는 곳입니다 
+//Winey의 디자인 컴포넌트를 모아 두는 곳입니다
+//빈 파일입니다
 
 public final class WineyKit {
-    public static func hello() {
-        print("Hello, from your Kit framework")
-    }
-  
-  public let a = "asd"
-  
-  func test() {
-  }
-  
   public init() {}
-  
 }
