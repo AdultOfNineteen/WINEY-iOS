a0e5110ab68a023ef72bbc01acf14edeffaedf18
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-08-08 23:16:13 +0900

    [WIN-62][#8]️✏️Lint적용 및 수정

--- Projects/App/Sources/AppRoot/WineyApp.swift
+++ Projects/App/Sources/AppRoot/WineyApp.swift
@@ -5,13 +5,12 @@ import WineyKit
 struct WineyApp: App {
   
   init() {
-    WineyKitFontFamily.registerAllCustomFonts() //프로젝트 전체에서 공유할 수 있도록
+    WineyKitFontFamily.registerAllCustomFonts() // 프로젝트 전체에서 공유할 수 있도록
   }
   
   var body: some Scene {
     WindowGroup {
       ContentView()
-      
     }
   }
 }
