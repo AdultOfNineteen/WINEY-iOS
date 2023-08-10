153753a70418963d30ae1eea5456b84c1708c93a
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-08-10 12:30:04 +0900

    [Tuist][#2]️🔥ConbinExt, TCA, TCACoordinator 버전 고정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -7,9 +7,11 @@
 import ProjectDescription
 
 let spm = SwiftPackageManagerDependencies([
-  .remote(url: "https://github.com/CombineCommunity/CombineExt.git", requirement: .exact("1.0.0")),
-  .remote(url: "https://github.com/pointfreeco/swift-composable-architecture.git", requirement: .upToNextMajor(from: "0.35.0")),
-  .remote(url: "https://github.com/johnpatrickmorgan/TCACoordinators.git", requirement: .upToNextMajor(from: "0.2.0")),
+  .remote(
+    url: "https://github.com/CombineCommunity/CombineExt.git", requirement: .exact("1.8.1")
+  ),
+  .remote(url: "https://github.com/pointfreeco/swift-composable-architecture.git", requirement: .exact("0.40.2")),
+  .remote(url: "https://github.com/johnpatrickmorgan/TCACoordinators.git", requirement: .exact("0.2.0")),
 ])
 
 let dependencies = Dependencies(
