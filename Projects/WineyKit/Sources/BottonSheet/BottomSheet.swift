e616b870a8306977ab4db8f8957e01cd6661234b
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-08-21 19:39:36 +0900

    [WIN-63][#4]️✨BottomSheet 생성

---
+++ Projects/WineyKit/Sources/BottonSheet/BottomSheet.swift
@@ -0,0 +1,123 @@
+//
+//  BottomSheet.swift
+//  WineyKit
+//
+//  Created by 박혜운 on 2023/08/19.
+//  Copyright © 2023 com.adultOfNineteen. All rights reserved.
+//
+
+import SwiftUI
+
+public struct BottomSheet<
+  BackgroundColor: View,
+  HeaderArea: View,
+  Content: View,
+  BottomArea: View
+>: View {
+  private var backgroundColor: BackgroundColor
+  @Binding private var isPresented: Bool
+  private var headerArea: HeaderArea
+  private var content: Content
+  private var bottomArea: BottomArea
+  private let activeOutsideTouch: Bool
+  
+  private let keyWindow = UIApplication.shared.connectedScenes
+    .compactMap { $0 as? UIWindowScene }
+    .flatMap { $0.windows }
+    .first { $0.isKeyWindow }
+  
+  public init(
+    backgroundColor: BackgroundColor,
+    isPresented: Binding<Bool>,
+    activeOutsideTouch: Bool = false,
+    @ViewBuilder headerArea: () -> HeaderArea,
+    @ViewBuilder content: () -> Content,
+    @ViewBuilder bottomArea: () -> BottomArea
+  ) {
+    self.backgroundColor = backgroundColor
+    self._isPresented = isPresented
+    self.activeOutsideTouch = activeOutsideTouch
+    self.headerArea = headerArea()
+    self.content = content()
+    self.bottomArea = bottomArea()
+  }
+  
+  public var body: some View {
+    VStack(spacing: 0) {
+      headerArea
+        .padding(.top, 32)
+      
+      content
+        .padding(.horizontal, 24)
+        .padding(.top, 32)
+      
+      bottomArea
+        .padding(.top, 24)
+      
+      Spacer()
+        .frame(height: keyWindow?.safeAreaInsets.bottom)
+        .padding(.top, 8)
+    }
+    .background(backgroundColor)
+  }
+}
+
+public extension View {
+  func bottomSheet<
+    BackgroundColor: View,
+    HeaderArea: View,
+    Content: View,
+    BottomArea: View
+  >(
+    backgroundColor: BackgroundColor = Color.black,
+    isPresented: Binding<Bool>,
+    activeOutsideTouch: Bool = false,
+    @ViewBuilder headerArea: () -> HeaderArea,
+    @ViewBuilder content: () -> Content,
+    @ViewBuilder bottomArea: () -> BottomArea
+  ) -> some View {
+    ZStack {
+      self
+      
+      ZStack(alignment: .bottom) {
+        if isPresented.wrappedValue {
+          WineyKitAsset.gray950.swiftUIColor.opacity(0.85)
+            .ignoresSafeArea(.container, edges: .all)
+            .zIndex(1)
+            .onTapGesture {
+              if activeOutsideTouch {
+                isPresented.wrappedValue = false
+              }
+            }
+            .transition(.opacity)
+          
+          BottomSheet(
+            backgroundColor: backgroundColor,
+            isPresented: isPresented,
+            headerArea: headerArea,
+            content: content,
+            bottomArea: bottomArea
+          )
+          .zIndex(2)
+          .transition(.move(edge: .bottom))
+        }
+      }
+      .ignoresSafeArea()
+    }
+    .animation(.easeInOut(duration: 0.1), value: isPresented.wrappedValue)
+  }
+}
+
+private struct RoundCorners: Shape {
+  var radius: CGFloat = 20
+  var corners: UIRectCorner = [.topLeft, .topRight]
+  
+  func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(
+      roundedRect: rect,
+      byRoundingCorners: corners,
+      cornerRadii: CGSize(width: radius, height: radius)
+    )
+    return Path(path.cgPath)
+  }
+}
