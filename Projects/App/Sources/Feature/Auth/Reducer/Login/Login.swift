2dec710e9a917777e8358c6e2809a00968208096
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-09-27 04:15:19 +0900

    [WIN-96][#36]✨카카오 소셜 Login 구성
    Reducer/Login.swift

---
+++ Projects/App/Sources/Feature/Auth/Reducer/Login/Login.swift
@@ -0,0 +1,116 @@
+//
+//  LoginCore.swift
+//  Winey
+//
+//  Created by 박혜운 on 2023/08/10.
+//  Copyright © 2023 com.adultOfNineteen. All rights reserved.
+//
+
+import Combine
+import ComposableArchitecture
+import Foundation
+
+public struct Login: Reducer {
+  
+  public struct State: Equatable {
+    var loginPath: LoginPathType?
+  }
+  
+  public enum Action {
+    // MARK: - User Action
+    case tappedLogin(LoginPathType)
+    case tappedTermsOfUse
+    
+    // MARK: - Inner Business Action
+    case _onAppear
+    case _checkLoginHistory
+    case _socialLogin(path: LoginPathType, accessToken: String?)
+    
+    // MARK: - Inner SetState Action
+    case _setLoginPath(LoginPathType)
+    case _completeSocialNetworking(Result<LoginUserDTO, Error>)
+    case _moveUserStatusPage(LoginProcessType)
+  }
+  
+  @Dependency(\.userDefaults) var userDefaultsService
+  @Dependency(\.auth) var authService
+  
+  public func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    switch action {
+    case ._onAppear:
+      return .send(._checkLoginHistory)
+      
+    case ._checkLoginHistory:
+      if let path = userDefaultsService.loadValue(.socialLoginPath) {
+        switch path {
+        case "kakao":
+          return .send(._setLoginPath(.kakao))
+        case "google":
+          return .send(._setLoginPath(.google))
+        case "APPLE":
+          return .send(._setLoginPath(.apple))
+        default: break
+        }
+      }
+      
+      return .none
+      
+    case .tappedLogin(let path):
+      if path == .google {
+        return .send(._moveUserStatusPage(.done)) // 구글버튼 홈 이동용
+      }
+      return .run { send in
+        if let token = await authService.socialLogin(path) {
+          await send(._socialLogin(path: path, accessToken: token))
+        }
+      }
+      
+    case .tappedTermsOfUse:
+      return .none
+      
+    case let ._setLoginPath(path):
+      state.loginPath = path
+      return .none
+      
+    case let ._socialLogin(path, token):
+      self.userDefaultsService.saveValue(.socialLoginPath, path.rawValue)
+      return .run { send in
+        if let token = token {
+          let data = await authService.loginState(path, token)
+          await send(._completeSocialNetworking(data))
+        }
+      }
+
+    case ._completeSocialNetworking(.failure):
+      return .none // 에러 처리
+      
+    case let ._completeSocialNetworking(.success(data)):
+      sendUserDefaults(data: data)
+      
+      return .send(
+        ._moveUserStatusPage(
+          self.routeBasedOnLoginStatus(data: data)
+        )
+      )
+      
+    default: return .none
+    }
+  }
+  
+  private func sendUserDefaults(data: LoginUserDTO) {
+    self.userDefaultsService.saveValue(.accessToken, data.accessToken)
+    self.userDefaultsService.saveValue(.refreshToken, data.refreshToken)
+    self.userDefaultsService.saveValue(.userID, String(data.userId))
+  }
+  
+  private func routeBasedOnLoginStatus(data: LoginUserDTO) -> LoginProcessType {
+    switch (data.userStatus, data.messageStatus) {
+    case ("ACTIVE", _):
+      return .done
+    case (_, "VERIFIED"):
+      return .flavor
+    default:
+      return .code
+    }
+  }
+}
