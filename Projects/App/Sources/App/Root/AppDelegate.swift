cd5634006aaed90d47da986c232dded3a47b5809
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-09-27 04:10:38 +0900

    [WIN-96][#36]✨카카오 로그인 위한 AppDelegate 설정

--- Projects/App/Sources/App/Root/AppDelegate.swift
+++ Projects/App/Sources/App/Root/AppDelegate.swift
@@ -7,24 +7,42 @@
 //
 
 import Foundation
+import KakaoSDKAuth
+import KakaoSDKCommon
 import SwiftUI
 import UIKit
+import WineyNetwork
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
   func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:])
+  -> Bool {
+    if AuthApi.isKakaoTalkLoginUrl(url) {
+      return AuthController.handleOpenUrl(url: url)
+    }
+    return false
+  }
+  
+  func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    KakaoSDK.initSDK(appKey: APIKeys.kakaoAppKey)
+    
     self.window = UIWindow(frame: UIScreen.main.bounds)
     let appView = AppCoordinatorView(
       store: .init(
         initialState: .initialState,
         reducer: { AppCoordinator() })
     )
-  
+    
     window?.rootViewController = UIHostingController(rootView: appView)
     window?.makeKeyAndVisible()
     
