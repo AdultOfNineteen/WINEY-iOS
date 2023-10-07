e6b7c2e2e9f1ab16232efb13e785a25af75a3c4b
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-10-07 21:16:19 +0900

    [WIN-117][#36]✏️BottomSheet 주석 삭제

--- Projects/App/Sources/Feature/Auth/View/BottomSheet/SignUpBottomSheetContent.swift
+++ Projects/App/Sources/Feature/Auth/View/BottomSheet/SignUpBottomSheetContent.swift
@@ -52,50 +52,6 @@ struct SignUpBottomSheetContent: View {
   }
 }
 
-//struct CodeSignUpBottomSheetContent: View {
-//  private let store: StoreOf<CodeSignUp>
-//
-//  init(store: StoreOf<CodeSignUp>) {
-//    self.store = store
-//  }
-//
-//  var body: some View {
-//    WithViewStore(store, observe: \.bottomSheetType) { viewStore in
-//      switch viewStore.state {
-//
-//      case .back:
-//        CustomVStack(text1: "진행을 중단하고 처음으로", text2: "되돌아가시겠어요?")
-//
-//      case let .alreadySignUp((phone, type)):
-//        VStack(spacing: 2.4) {
-//          Text("\(phone)님은")
-//            .foregroundColor(WineyKitAsset.gray200.swiftUIColor)
-//          HStack(spacing: 5) {
-//            Text("\(type.rawValue) 소셜")
-//              .foregroundColor(WineyKitAsset.main3.swiftUIColor)
-//            Text("회원으로")
-//              .foregroundColor(WineyKitAsset.gray200.swiftUIColor)
-//          }
-//          Text("가입하신 기록이 있어요")
-//            .foregroundColor(WineyKitAsset.gray200.swiftUIColor)
-//        }
-//        .wineyFont(.bodyB1)
-//
-//      case .codeFail:
-//        CustomVStack(text1: "인증에 실패했어요", text2: "처음부터 다시 입력해주세요!")
-//
-//      case .sendCode:
-//        VStack(spacing: 14) {
-//          CustomVStack(text1: "인증번호가 발송되었어요", text2: "3분 안에 인증번호를 입력해주세요")
-//          Text("*인증번호 요청 3회 초과 시 5분 제한")
-//            .wineyFont(.captionM2)
-//            .foregroundColor(WineyKitAsset.gray600.swiftUIColor)
-//        }
-//      }
-//    }
-//  }
-//}
-
 struct CustomVStack: View {
   let text1: String
   let text2: String
--- Projects/App/Sources/Feature/Auth/View/BottomSheet/SignUpBottomSheetFooter.swift
+++ Projects/App/Sources/Feature/Auth/View/BottomSheet/SignUpBottomSheetFooter.swift
@@ -18,12 +18,13 @@ struct SignUpBottomSheetFotter: View {
   private let tappedCodeFailConfirm: (() -> Void)
   private let tappedSendCodeConfirm: (() -> Void)
   
-  init(bottomSheetType: SignUpBottomSheetType,
-       tappedBackButtonNoOption: @escaping (() -> Void) = {},
-       tappedBackButtonYesOption: @escaping (() -> Void) = {},
-       tappedAlreadySignUpConfirm: @escaping (() -> Void) = {},
-       tappedCodeFailConfirm: @escaping (() -> Void) = {},
-       tappedSendCodeConfirm: @escaping (() -> Void) = {}
+  init(
+    bottomSheetType: SignUpBottomSheetType,
+    tappedBackButtonNoOption: @escaping (() -> Void) = {},
+    tappedBackButtonYesOption: @escaping (() -> Void) = {},
+    tappedAlreadySignUpConfirm: @escaping (() -> Void) = {},
+    tappedCodeFailConfirm: @escaping (() -> Void) = {},
+    tappedSendCodeConfirm: @escaping (() -> Void) = {}
   ) {
     self.bottomSheetType = bottomSheetType
     self.tappedBackButtonNoOption = tappedBackButtonNoOption
@@ -39,15 +40,9 @@ struct SignUpBottomSheetFotter: View {
         TwoOptionSelectorButtonView(
           leftTitle: "아니오",
           leftAction: { self.tappedBackButtonNoOption() }
-          //              {
-          // viewStore.send(._presentBottomSheet(false))
-          //            }
           ,
           rightTitle: "예",
           rightAction: { self.tappedBackButtonYesOption() }
-          //              {
-          //              viewStore.send(._backToFirstView)
-          //            }
         )
       }
       
@@ -58,9 +53,6 @@ struct SignUpBottomSheetFotter: View {
           title: "확인",
           validBy: true,
           action: { self.tappedAlreadySignUpConfirm() }
-          //              {
-          //              viewStore.send(.tappedBottomAlreadySignUpButton)
-          //            }
         )
       }
       
@@ -69,9 +61,6 @@ struct SignUpBottomSheetFotter: View {
           title: "확인",
           validBy: true,
           action: { self.tappedCodeFailConfirm() }
-          //              {
-          //              viewStore.send(.tappedBottomFailConfirmButton)
-          //            }
         )
       }
       
@@ -81,68 +70,9 @@ struct SignUpBottomSheetFotter: View {
           validBy: true,
           action: {
             self.tappedSendCodeConfirm()
-            // viewStore.send(.tappedBottomResendCodeButton)
           }
         )
       }
     }
   }
 }
-
-//stzuct CodeSignUpBottomSheetFooter: View {
-//  private let store: StoreOf<CodeSignUp>
-//
-//
-//  init(store: StoreOf<CodeSignUp>) {
-//    self.store = store
-//  }
-//
-//  var body: some View {
-//    WithViewStore(store, observe: \.bottomSheetType) { viewStore in
-//      VStack {
-//        if viewStore.state == .back {
-//          TwoOptionSelectorButtonView(
-//            leftTitle: "아니오",
-//            leftAction: {
-//              viewStore.send(._presentBottomSheet(false))
-//            },
-//            rightTitle: "예",
-//            rightAction: {
-//              viewStore.send(._backToFirstView)
-//            }
-//          )
-//        }
-//
-//        if case .alreadySignUp = viewStore.state {
-//          WineyConfirmButton(
-//            title: "확인",
-//            validBy: true,
-//            action: {
-//              viewStore.send(.tappedBottomAlreadySignUpButton)
-//            }
-//          )
-//        }
-//
-//        if viewStore.state == .codeFail {
-//          WineyConfirmButton(
-//            title: "확인",
-//            validBy: true,
-//            action: {
-//              viewStore.send(.tappedBottomFailConfirmButton)
-//            }
-//          )
-//        }
-//
-//        if viewStore.state == .resendCode {
-//          WineyConfirmButton(
-//            title: "확인",
-//            validBy: true,
-//            action: {
-//              viewStore.send(.tappedBottomResendCodeButton)
-//            }
-//          )
-//        }
-//      }
-//    }
-//  }
-//}
