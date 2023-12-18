c48b6df94ea41fb77735bc05170b01b0df44c8d1
JungDohyeon <jdh1109ok@naver.com>
2023-12-19 01:35:26 +0900

    [WIN-162][#51]✨NoteDTO

---
+++ Projects/App/Sources/Feature/Tab/Main/Data/DTO/NoteDTO.swift
@@ -0,0 +1,19 @@
+//
+//  NoteDTO.swift
+//  Winey
+//
+//  Created by 정도현 on 12/19/23.
+//  Copyright © 2023 Winey. All rights reserved.
+//
+
+import Foundation
+
+public struct NoteDTO: Codable, Equatable {
+
+  let noteId: Int
+  let wineName: String
+  let country: String
+  let starRating: Int
+  let buyAgain: Bool
+  let wineType: String
+}
