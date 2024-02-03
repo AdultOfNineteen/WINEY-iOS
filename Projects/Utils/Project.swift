94eb3e4b97dfc50c9e242154441b62c4973fdc71
hyeun <81788774+shippingpark@users.noreply.github.com>
2024-02-04 04:04:46 +0900

    [WIN-173][#63]🗑️ 불필요 주석 삭제

--- Projects/Utils/Project.swift
+++ Projects/Utils/Project.swift
@@ -10,7 +10,6 @@ let project = Project.make(
       name: "Utils",
       product: .framework,
       bundleId: "com.winey.utils",
-      deploymentTarget: .iOS(targetVersion: "16.0", devices: [.iphone]),
       sources: ["Sources/**"],
       dependencies: [
         .project(target: "ThirdPartyLibs", path: "../ThirdPartyLibs")
