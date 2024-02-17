cbca43a0c74908ba1755f0be71ad1cb44975383e
JungDohyeon <40754281+JungDohyeon@users.noreply.github.com>
2024-02-18 04:04:21 +0900

    [QA] 회원탈퇴 바텀시트 아이콘 적용

--- Projects/App/Sources/UserInfo/View/SignOutConfirmView.swift
+++ Projects/App/Sources/UserInfo/View/SignOutConfirmView.swift
@@ -40,7 +40,7 @@ public struct SignOutConfirmView: View {
         send: .tappedOutsideSheet
       ),
       headerArea: {
-        BadgeBottomSheetHeader()
+        Image("analysisNoteIcon")
       },
       content: {
         Text("탈퇴가 완료되었어요\n언젠가 다시 만나요!")
