0994f04137da719ccc17a636f9dfb8093b194b5a
bono <shippingbok@gmail.com>
2024-04-25 10:52:19 +0900

    [WIN-173][#63]🔨marker 탭 구독 취소 처리

--- Projects/App/Sources/Map/Reducer/Map.swift
+++ Projects/App/Sources/Map/Reducer/Map.swift
@@ -14,16 +14,14 @@ public struct Map: Reducer {
   public init() {}
   
   public struct State: Equatable {
-    // MARK: - Map
-    var coordinator: NaverMapCoordinator = NaverMapCoordinator.shared
-    
     // MARK: - Visual Data
     @BindingState var filterCategory: ShopCategoryType = .all
     
     // MARK: - BottomSheetState
-    var mapSheet: MapSheet.State = .init(shopList: [.dummy, .dummy2])
+    var mapSheet: MapSheet.State = .init(shopList: [])
     var sheetHeight: ShopSheetHeight = .close
     
+    var tappedOpenFirst: Bool = true
     var moveNavigation: Bool = false
     var setProgressView: Bool = false
     
@@ -42,81 +40,58 @@ public struct Map: Reducer {
     case tappedReloadCurrentMap
     
     // MARK: - Inner Business Action
-    case _onAppear
-    case _onDisappear
     case _checkLocation
-    case _userLocationIsEnabled(Bool)
-    case _mapMovementDetection
+    case _userLocationIsEnabled(Bool) // 미사용
     case _getShopInfo
     case _setShopInfo([ShopMapDTO])
     case _setMapMarker([ShopMapDTO])
     case _changeBottomSheet(height: ShopSheetHeight)
     case _activeProgressView(Bool)
-    case _moveNavigationView(Bool)
     case _networkError
     
     // MARK: - Inner SetState Action
     case _tabBarHidden
     case _tabBarOpen
+    case _tappedMapTabBarItem(Bool)
   }
   
   @Dependency(\.mainQueue) var mainQueue
   @Dependency(\.continuousClock) var clock
   @Dependency(\.alert) var alertService
   @Dependency(\.map) var mapService
-  @Dependency(\.naverCoordinator) var naverMapCoordinator
-
+  @Dependency(\.location) var locationService
+  @Dependency(\.naverCoordinator) var naverMapService
+  
   public var body: some ReducerOf<Self> {
     BindingReducer()
     
     Reduce { state, action in
       switch action {
-        
-//      case .binding(\.$filterCategory):
-//        print("🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷🇰🇷")
-//        print("필터 변경에 따라 움직임")
-//        return .send(._getShopInfo)
-        
-      case ._onAppear:
-        return .send(._checkLocation)
+      case let ._tappedMapTabBarItem(isTappedMapTabBarItem):
+        guard !state.tappedOpenFirst else {
+          state.tappedOpenFirst = false
+          return .send(._checkLocation)
+        }
+        guard isTappedMapTabBarItem else { return .none }
+        return .cancel(id: CancelID.mapMarker)
         
       case ._checkLocation:
-        let coordinator = state.coordinator
         return .run { send in
-          let result = await coordinator.checkIfLocationServiceIsEnabled()
-          await send(._userLocationIsEnabled(result))
+          _ = await locationService.checkIfLocationServiceIsEnabled()
         }
-        
-      case let ._userLocationIsEnabled(result):
-        if result {
-          return .send(._getShopInfo)
-        } else {
-          return .none // 경고창 출력
-        }
-        
-//      case ._mapMovementDetection:
-//        
-//        return .run { send in
-//          for await _ in await naverMapCoordinator.cameraMovement() {
-//            print("카메라 이동 받고 있음")
-//            await send(._getShopInfo)
-//          }
-//        }
         
       case ._getShopInfo:
-        print("1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣")
-        let coordinator = state.coordinator
-        let cameraEdge = coordinator.getCameraAnglePostion()
-        let cameraCenter = coordinator.getCameraCenterPosition()
+        let cameraEdge = naverMapService.getCameraAnglePostion()
+        let cameraCenter = naverMapService.getCameraCenterPosition()
         let category = state.filterCategory
         return  .run { send in
           let result = await mapService.getShopsInfoOf(
-            category, 
+            category,
             cameraCenter.latitude,
             cameraCenter.longitude,
-            cameraEdge.leftTopLatitude, 
+            cameraEdge.leftTopLatitude,
             cameraEdge.leftTopLongitude,
-            cameraEdge.rightBottomLatitude, 
+            cameraEdge.rightBottomLatitude,
             cameraEdge.rightBottomLongitude
           )
           
@@ -126,16 +101,10 @@ public struct Map: Reducer {
             await send(._setMapMarker(data))
             return
           case .failure:
-            print("실패?")
             return await send(._networkError)
           }
         }
-//        .debounce(
-//          id: CancelID.mapCameraObserver,
-//          for: 1.0,
-//          scheduler: self.mainQueue
-//        )
-      
+        
       case let ._setShopInfo(data):
         state.mapSheet.shopList = IdentifiedArray(uniqueElements: data)
         return .run { send in
@@ -145,17 +114,21 @@ public struct Map: Reducer {
         
       case let ._setMapMarker(data):
         return .run { send in
-          for await id in await naverMapCoordinator.tappedMarker(data) {
-            print("마커 선택 리듀서에서 읽음")
+          for await id in await naverMapService.tappedMarker(data) {
             await send( .mapSheet(.tappedShopListCell(id: id)))
+            await send(._changeBottomSheet(height: .medium))
           }
         }
+        .cancellable(id: CancelID.mapMarker)
         
       case .tappedCurrentUserLocationMarker:
-        
-        state.coordinator.fetchUserLocation() // dlr
-        
-        return .send(._getShopInfo)
+        if let location = locationService.userLocation() {
+          naverMapService.updateUserLocationInMap(location.lat, location.lng)
+        } else {
+          // MARK: - 내 위치 허용 권장
+          alertService.showAlert("내 위치를 알기 위해 위치접근 허용이 필요해요!")
+        }
+        return .none
         
       case let .mapSheet(.tappedShopBusinessHour(open)):
         state.sheetHeight = open ? .large : .medium
@@ -166,28 +139,7 @@ public struct Map: Reducer {
         return .none
         
       case .tappedReloadCurrentMap:
-        alertService.showAlert("아직... 연결되지 못한 기능입니다😭")
-        
         return .send(._getShopInfo)
-        
-//      case let .mapSheet(.tappedBookMark(id)):
-        //        .tappedBookMark(index):
-        // API 호출
-        // 성공하면 값 변경 후 알림창 띄우기
-        
-//        guard let old = state.mapSheet.shopList.first(where: { $0.id == id }) else {return .none}
-//        guard let index = state.mapSheet.shopList.firstIndex(of: old) else {return .none}
-//        
-//        let new = old.changeBookMarkState()
-//        state.mapSheet.shopList[index] = new
-//
-//        return .run { send in
-//          if !new.like {
-//            alertService.showAlert("내 장소에서 삭제하였습니다")
-//          }
-//        }
-        
-//        return .none
         
       case .mapSheet(.tappedNavigationBackButton):
         switch state.mapSheet.destination {
@@ -219,9 +171,7 @@ public struct Map: Reducer {
           return .run { send in
             await send(._tabBarHidden)
             await send(._activeProgressView(true))
-            await send(._getShopInfo) // 추가
-//              .send(._tabBarHidden)
-//            await send(._activeProgressView(false))
+            await send(._getShopInfo)
           }
         }
         
@@ -233,7 +183,7 @@ public struct Map: Reducer {
           state.sheetHeight = .medium
         }
         return .none
-
+        
       default: return .none
       }
     }
