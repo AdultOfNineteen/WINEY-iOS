56965205f4b40d5ddd4013c6b0d56ec2353c96eb
bono <shippingbok@gmail.com>
2024-04-25 10:51:44 +0900

    [WIN-173][#63]🔨LocationService NaverMap 분리

--- Projects/App/Sources/Map/Services/NaverMapCoordinatorService.swift
+++ Projects/App/Sources/Map/Services/NaverMapCoordinatorService.swift
@@ -29,9 +29,13 @@ public struct NaverMapCoordinatorService {
     _ shopId: Int
   ) async -> Result<VoidResponse, Error>
   
-  public var updateUserLocationInMap: () -> ()
+  public var updateUserLocationInMap: (_ lat: Double, _ lng: Double) -> Void
   
   public var tappedMarker: @Sendable ([ShopMapDTO]) async -> AsyncStream<Int>
+  
+  public var getCameraCenterPosition: () -> MapPosition
+  
+  public var getCameraAnglePostion: () -> MapCameraAnglePosition
 }
 
 extension NaverMapCoordinatorService {
@@ -65,18 +69,19 @@ extension NaverMapCoordinatorService {
             MapAPI.bookmark(shopId: shopId),
             type: VoidResponse.self
           )
-      }, 
-      updateUserLocationInMap: {
-        naverMapCoordinator.fetchUserLocation()
+      },
+      updateUserLocationInMap: { lat, lng in
+        naverMapCoordinator.fetchUserLocation(lat, lng)
       },
       tappedMarker: { shopList in
         await naverMapCoordinator.setShopMarker(shopList)
+      },
+      getCameraCenterPosition: {
+        return naverMapCoordinator.getCameraCenterPosition()
+      },
+      getCameraAnglePostion: {
+        return naverMapCoordinator.getCameraAnglePostion()
       }
-//      ,
-//      cameraMovement: {
-//        naverMapCoordinator.cameraMovement()
-//        
-//      }
     )
   }()
   //  static let mock = Self(…)
@@ -97,22 +102,16 @@ extension DependencyValues {
 public final class NaverMapCoordinator:
   NSObject,
   NMFMapViewCameraDelegate,
-  NMFMapViewTouchDelegate,
-  CLLocationManagerDelegate {
+  NMFMapViewTouchDelegate
+{
   
   static let shared = NaverMapCoordinator()
   
-  let view = NMFNaverMapView(frame: .zero)
+  private let view = NMFNaverMapView(frame: .zero)
+  private var markers: [NMFMarker] = []
   private var clickedMarkerContinuation: AsyncStream<Int>.Continuation?
   
-  enum MapConstants {
-    static let dotMarkerSize: (widht: CGFloat, height: CGFloat) = (29, 29)
-    static let clickedMarkerSize: (widht: CGFloat, height: CGFloat) = (43.29, 63)
-    static let dotMarkerImageName: String = "dot_marker"
-    static let clickedMarkerImageName: String = "clicked_marker"
-  }
-  
-  public override init() {
+  private override init() {
     super.init()
     
     view.mapView.positionMode = .direction
@@ -132,98 +131,50 @@ public final class NaverMapCoordinator:
     view.mapView.touchDelegate = self
   }
   
+  func getNaverMapView() -> NMFNaverMapView {
+    view
+  }
+  
   @MainActor
-  public func setShopMarker(_ shopList: [ShopMapDTO]) -> AsyncStream<Int> {
-    for shop in shopList {
-      let marker = NMFMarker()
-      marker.position = NMGLatLng(lat: shop.latitude, lng: shop.longitude)
-      marker.mapView = view.mapView
-      marker.iconImage = NMFOverlayImage(name: shop.shopMarkerType.imageTitle)
-      marker.width = 29 // 닷
-      marker.height = 29
-      
-      marker.touchHandler = { [weak self] (overlay: NMFOverlay) -> Bool in
-//        marker.iconImage = NMFOverlayImage(name: "clicked_marker")
-//        marker.width = 43.29 // 닷
-//        marker.height = 63
-        self?.clickedMarkerContinuation?.yield(shop.id)
-        return true
-      }
-    }
+  fileprivate func setShopMarker(_ shopList: [ShopMapDTO]) -> AsyncStream<Int> {
+    markers.forEach({ $0.mapView = nil })
+    markers = []
     
-    return setTappedMarkerEvent()
-  }
-  
-  private func setTappedMarkerEvent() -> AsyncStream<Int> {
-    return AsyncStream<Int> { continuation in
-      self.clickedMarkerContinuation = continuation
-    }
-  }
-  
-//  public func cameraMovement() -> AsyncStream<Void> {
-//    return AsyncStream<Void> { continuation in
-//      // Continuation을 저장하여 mapView delegate 메소드에서 이벤트를 방출할 수 있도록 합니다.
-//      self.cameraMovementContinuation = continuation
-//    }
-//  }
-  
-//  public func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
-//    // 카메라 이동이 시작되기 전 호출되는 함수
-//  }
-//  
-//  public func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
-//    // 카메라의 위치가 변경되면 호출되는 함수
-////    cameraMovementContinuation?.yield(())
-//  }
-  
-  
-  
-//  public func mapViewCameraIdle(_ mapView: NMFMapView) {
-////    cameraMovedAction()
-//    // 여기서 API 호출 뒤 마커 뷰 깔기 위해 뷰 갱신
-////    debounceWorkItem?.cancel() // 이전에 예약된 작업이 있다면 취소
-////    let workItem = DispatchWorkItem { [weak self] in // 새로운 작업 생성
-//    self.cameraMovementContinuation?.yield(()) // 이벤트 전달
-////    }
-////    
-////    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: workItem)
-////    
-////    debounceWorkItem = workItem 
-//    // 1. 현 지도 위치의 음식점들 API 메서드 호출
-//    // 2. 마커뷰 세팅 메서드 호출
-//  }
-  
-  var locationManager: CLLocationManager?
-  
-  // MARK: - 위치 정보 동의 확인
-  func checkLocationAuthorization() async -> Bool {
-    guard let locationManager = locationManager else { return false }
-    return await withCheckedContinuation { continuation in
-      switch locationManager.authorizationStatus {
-      case .notDetermined:
-        locationManager.requestWhenInUseAuthorization()
-      case .restricted:
-        print("위치 정보 접근이 제한되었습니다.")
-      case .denied:
-        print("위치 정보 접근을 거절했습니다. 설정에 가서 변경하세요.")
-      case .authorizedAlways, .authorizedWhenInUse:
-        print("Success")
-        continuation.resume(returning: true)
-        return
-      @unknown default: break
+    if shopList.isEmpty {
+      return .finished
+    } else {
+      for shop in shopList {
+        let marker = NMFMarker()
+        marker.position = NMGLatLng(lat: shop.latitude, lng: shop.longitude)
+        marker.mapView = view.mapView
+        marker.iconImage = NMFOverlayImage(name: shop.shopMarkerType.imageTitle)
+        marker.width = 29 // 닷
+        marker.height = 29
+        marker.touchHandler = { [weak self] (overlay: NMFOverlay) -> Bool in
+          
+        // MARK: - 마커 탭 이미지 미적용 (디자이너에게 탭 마커 이미지 요청)
+        //    marker.iconImage = NMFOverlayImage(name: "clicked_marker") // 마커 탭 시 이미지 미적용
+        //    marker.width = 43.29 // 닷
+        //    marker.height = 63
+          
+          self?.clickedMarkerContinuation?.yield(shop.id)
+          return true
+        }
+        markers.append(marker)
       }
-      continuation.resume(returning: false)
+      
+      return setTappedMarkerEvent()
     }
   }
   
-  func getCameraCenterPosition() -> MapPosition {
+  fileprivate func getCameraCenterPosition() -> MapPosition {
     let centerLat = view.mapView.cameraPosition.target.lat
     let centerLng = view.mapView.cameraPosition.target.lng
     
     return .init(latitude: centerLat, longitude: centerLng)
   }
   
-  func getCameraAnglePostion() -> MapCameraAnglePosition {
+  fileprivate func getCameraAnglePostion() -> MapCameraAnglePosition {
     let southWest = view.mapView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest // 남서
     let northEast = view.mapView.projection.latlngBounds(fromViewBounds: self.view.frame).northEast // 북동
     
@@ -232,56 +183,37 @@ public final class NaverMapCoordinator:
       rightBottomLatitude: southWest.lat, rightBottomLongitude: northEast.lng // 동남
     )
   }
-  
-  // Coordinator 클래스 안의 코드
-  func checkIfLocationServiceIsEnabled() async -> Bool {
-    print("1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣")
-    return await withCheckedContinuation { continuation in
-      print("2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣2️⃣")
-      if CLLocationManager.locationServicesEnabled() {
-        print("3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣3️⃣")
-        Task { @MainActor in
-          print("4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣4️⃣")
-          //      DispatchQueue.main.async {
-          self.locationManager = CLLocationManager()
-          self.locationManager!.delegate = self
-          print("5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣5️⃣")
-          let result =  await self.checkLocationAuthorization()
-          print("6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣6️⃣")
-          continuation.resume(returning: result)
-          print("\(result) 결과 값은??????")
-        }
-      }
-      else {
-        print("Show an alert letting them know this is off and to go turn i on")
-        continuation.resume(returning: false)
-      }
-    }
+
+  fileprivate func fetchUserLocation(_ lat: Double, _ lng: Double) {
+    let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng), zoomTo: 15)
+    cameraUpdate.animation = .easeIn
+    cameraUpdate.animationDuration = 1
+    
+    let locationOverlay = view.mapView.locationOverlay
+    locationOverlay.location = NMGLatLng(lat: lat, lng: lng)
+    locationOverlay.hidden = false
+    
+    locationOverlay.icon = NMFOverlayImage(name: "location_overlay_icon")
+    locationOverlay.iconWidth = CGFloat(NMF_LOCATION_OVERLAY_SIZE_AUTO)
+    locationOverlay.iconHeight = CGFloat(NMF_LOCATION_OVERLAY_SIZE_AUTO)
+    locationOverlay.anchor = CGPoint(x: 0.5, y: 1)
+    
+    view.mapView.positionMode = .direction
+    view.mapView.moveCamera(cameraUpdate)
   }
   
-  func fetchUserLocation() {
-    if let locationManager = locationManager {
-      let lat = locationManager.location?.coordinate.latitude
-      let lng = locationManager.location?.coordinate.longitude
-      let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat ?? 0.0, lng: lng ?? 0.0), zoomTo: 15)
-      cameraUpdate.animation = .easeIn
-      cameraUpdate.animationDuration = 1
-      
-      let locationOverlay = view.mapView.locationOverlay
-      locationOverlay.location = NMGLatLng(lat: lat ?? 0.0, lng: lng ?? 0.0)
-      locationOverlay.hidden = false
-      
-      locationOverlay.icon = NMFOverlayImage(name: "location_overlay_icon")
-      locationOverlay.iconWidth = CGFloat(NMF_LOCATION_OVERLAY_SIZE_AUTO)
-      locationOverlay.iconHeight = CGFloat(NMF_LOCATION_OVERLAY_SIZE_AUTO)
-      locationOverlay.anchor = CGPoint(x: 0.5, y: 1)
-      
-      view.mapView.positionMode = .direction
-      view.mapView.moveCamera(cameraUpdate)
+  private func setTappedMarkerEvent() -> AsyncStream<Int> {
+    return AsyncStream<Int> { continuation in
+      self.clickedMarkerContinuation = continuation
     }
   }
-  
-  func getNaverMapView() -> NMFNaverMapView {
-    view
+}
+
+extension NaverMapCoordinator {
+  enum MapConstants {
+    static let dotMarkerSize: (widht: CGFloat, height: CGFloat) = (29, 29)
+    static let clickedMarkerSize: (widht: CGFloat, height: CGFloat) = (43.29, 63)
+    static let dotMarkerImageName: String = "dot_marker"
+    static let clickedMarkerImageName: String = "clicked_marker"
   }
 }
