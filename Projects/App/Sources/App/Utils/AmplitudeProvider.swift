169448286a33976bc6f740156244192b609511dc
JungDohyeon <jdh1109ok@naver.com>
2024-08-14 23:47:53 +0900

    [✨ Feat] Amplitude - YOUNG_BADGE_CLICK

--- Projects/App/Sources/App/Utils/AmplitudeProvider.swift
+++ Projects/App/Sources/App/Utils/AmplitudeProvider.swift
@@ -21,6 +21,7 @@ public enum AmplitudeEvent: String {
   // MyPage Event
   case MYPAGE_ENTER
   case WINEY_BADGE_CLICK
+  case YOUNG_BADGE_CLICK
 }
 
 final class AmplitudeProvider: ObservableObject {
