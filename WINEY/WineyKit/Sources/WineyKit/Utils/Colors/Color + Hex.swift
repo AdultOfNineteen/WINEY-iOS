d3014c73e0436c3d68de6aec006930bec3dd258e
JungDohyeon <jdh1109ok@naver.com>
2024-11-03 02:57:15 +0900

    [🔨 Refactor] Color Extension (Hex Code)

---
+++ WINEY/WineyKit/Sources/WineyKit/Utils/Colors/Color + Hex.swift
@@ -0,0 +1,44 @@
+//
+//  Color + Hex.swift
+//
+//
+//  Created by 정도현 on 11/3/24.
+//
+
+import SwiftUI
+
+extension Color {
+  public init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+  
+  public func toHex() -> String? {
+    let uic = UIColor(self)
+    guard let components = uic.cgColor.components, components.count >= 3 else {
+      return nil
+    }
+    let r = Float(components[0])
+    let g = Float(components[1])
+    let b = Float(components[2])
+    var a = Float(1.0)
+    
+    if components.count >= 4 {
+      a = Float(components[3])
+    }
+    
+    if a != Float(1.0) {
+      return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+    } else {
+      return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+    }
+  }
+}
