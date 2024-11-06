d2e1d622d03625501f67fbe370fa49863e13cbe9
JungDohyeon <jdh1109ok@naver.com>
2024-11-07 02:06:29 +0900

    [✨ Feat] 노트 조회 -  향 직접 추가

--- WINEY/Sources/Note/Screens/NoteDetail/NoteDetailView.swift
+++ WINEY/Sources/Note/Screens/NoteDetail/NoteDetailView.swift
@@ -111,7 +111,7 @@ extension NoteDetailView {
         if store.noteMode != .otherNotes {
           NoteDetailSmellFeatureView(
             circleColor: noteData.color,
-            smellKeywordList: Array(noteData.smellKeywordList)
+            smellKeywordList: Array(noteData.smellKeywordList) + Array(noteData.directKeywordList)
           )
           .padding(.horizontal, WineyGridRules.globalHorizontalPadding)
           
